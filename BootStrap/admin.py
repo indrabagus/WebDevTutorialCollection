4e3d5562ccadf3483951c4e9a4afd4af34bf754a
Indra Bagus <indra.bagus@gmail.com>
2017-02-27 14:55:23 +0700

    Main page finished

--- BootStrap/admin.py
+++ BootStrap/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CardMember
 
 # Register your models here.
+admin.site.register(CardMember)
--- BootStrap/models.py
+++ BootStrap/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class CardMember(models.Model):
+    identifier = models.IntegerField(primary_key=True,
+                                     blank=False,
+                                     null=False)
+    nama = models.CharField(max_length=255)
+    alamat = models.CharField(max_length=255)
+    email = models.EmailField(blank=False, null=True)
+    tempat_lahir = models.CharField(max_length=255)    
+    tgl_lahir = models.DateField()
+    phone = models.CharField(max_length=255,blank=True, null=True)
+    # photo = models.ImageField()
+
+    def __str__(self):
+        return str(self.num_id) + " -- " +self.nama
+
+    class Meta:
+        verbose_name = "Smart Card Member Profile"
